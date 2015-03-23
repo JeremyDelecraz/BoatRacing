d907c02d96d7a9cabdc93f58633311db29f603e4
Franfrandudu <francois.duvernaycfpt@gmail.com>
2015-03-23 13:35:09 +0100

    Ajout du fichier Sprite.js

---
+++ BoatRacing/script/Sprite.js
@@ -0,0 +1,9 @@
+/******************************************
+* Projet : Boat Racing
+* Description : Classe qui gère le Sprite
+* Date : 23/03/2015
+* Version : 1.0
+* Auteur : Duvernay François
+* *****************************************/
+
+
