64ffc158ed8e7c7bc220e9c43c011fa97f48fa81
Sebastian Lopez <sebastian.lopez.cfpt@gmail.com>
2015-03-23 16:27:06 +0100

    Base du code pour charger les images (pas encore functionnel)

--- BoatRacing/script/Loader.js
+++ BoatRacing/script/Loader.js
@@ -1,6 +1,42 @@
+/******************************************
+* Projet : Boat Racing
+* Description : Cette class va se charger de les images et de les mettres à disposition.
+* Date : 23.03.2015
+* Version : 1.0
+* Auteur : Sebastian Lopez
+* Tests : 
+* *****************************************/
+var self = this;
+function ObjLoader()
+{
+    this.images = array(id,Url),
+    this.total,
+    this.error,
+    this.count,
+    this.message;
+}
+
 function Loader(list)
 {
+    load = new ObjLoader();
+    var listOne = list;
     
+    var img = new Image();
+    
+    
+    img.onload = function ()
+    {
+        load.count += 1;
+    };
+    
+    img.onerror = function ()
+    {
+        load.message = "Erreur de chargement d'image";
+    };
+    
+    img.src = listOne.url;
+    
+    this.image[listOne.id] = img;
 }
 
 function Loaded()
@@ -10,7 +46,10 @@ function Loaded()
 
 function GetImage(id)
 {
-    
+    for(i = 0; i<load.total; i++)
+    {
+        return load.image[id];
+    }
 }
 
 
