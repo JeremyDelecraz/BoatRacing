85b8c25b601c27930a1112cf427af7ed5ef4a050
yejuse <j.delecraz@gmail.com>
2015-03-23 13:35:50 +0100

    Ajout de la class Game

--- BoatRacing/script/Game.js
+++ BoatRacing/script/Game.js
@@ -1,7 +1,78 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/******************************************
+ * Projet : Boat Racing
+ * Description : Classe qui gère le jeu
+ * Date : 23.03.2015
+ * Version : 1.0
+ * Auteur : Jeremy Delécraz
+ * *****************************************/
+
+/**
+ * 
+ * @returns {ObjGame}
  */
+function ObjGame()
+{
+    this.cars;
+    this.input;
+    this.map;
+    this.images;
+    this.pseudo;
 
+    this.can;
+    this.ctx;
+}
 
+/**
+ * 
+ * @param {type} canvas
+ * @returns {undefined}
+ */
+function Game(canvas)
+{
+    
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function TimerFct()
+{
+    
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function StateLoadMap()
+{
+
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function StateWaitStart()
+{
+
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function StatePlay()
+{
+
+}
+
+/**
+ * 
+ * @returns {undefined}
+ */
+function StateFinished()
+{
+
+}
