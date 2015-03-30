04d5dbe5b0d98bb0e698533a6b4097c50ee5afe7
Franfrandudu <francois.duvernaycfpt@gmail.com>
2015-03-30 14:06:47 +0200

    Mise a Jour Sprite.js, fonction Show, Move et Setanim

--- BoatRacing/script/Sprite.js
+++ BoatRacing/script/Sprite.js
@@ -1,18 +1,18 @@
 /******************************************
-* Projet : Boat Racing
-* Description : Classe qui gère le Sprite
-* Date : 23/03/2015
-* Version : 1.0
-* Auteur : Duvernay François
-* *****************************************/
+ * Projet : Boat Racing
+ * Description : Classe qui gère le Sprite
+ * Date : 23/03/2015
+ * Version : 1.0
+ * Auteur : Duvernay François
+ * *****************************************/
 
 function Sprite(ctx, img, x, y, frame, anim, rate) {
    this.x = x; //La position x du sprite (paramètre).
    this.y = y; //La position y du sprite (paramètre).
-   this.w; //La largeur du sprite.
-   this.h; //La hauteur du sprite.
-   this.alpha; //L'angle du sprite.
-   this.speed; //La vitesse du sprite.
+   this.w = 32; //La largeur du sprite.
+   this.h = 32; //La hauteur du sprite.
+   this.alpha = 0; //L'angle du sprite.
+   this.speed = 1; //La vitesse du sprite.
    this.img = img; //L'image du sprite (paramètre).
    this.sx = frame; //La position en x dans l'image.
    this.sy = anim; //La position en y dans l'image.
@@ -21,15 +21,45 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
    this.update; //Pour le réseau.
    this.ctx = ctx; //Contexte dans lequel va se trouver l'image (paramètre).
    this.Show = function() { //Fonction pour afficher le sprite.
-      this.ctx.drawImage(this.img, this.sx, this.sy, this.w, this.h, (this.x + 120), (this.y + 102), this.w, this.h);
+      //Si l'intervalle est égale à 100, on change la position dans le sprite.
+      if (this.rate === 100)
+      {
+         if (this.sx < 96)
+         {
+            this.sx += this.w;
+         }
+         else
+         {
+            this.sx = 0;
+         }
+         //On remet l'intervalle à 0.
+         this.rate = 0;
+      }
+      //Sauvegarde du contexte.
+      this.ctx.save();
+      this.ctx.translate(this.x, this.y);
+
+      //On fais tourner le sprite.
+      this.ctx.rotate(this.alpha);
+      this.ctx.drawImage(this.img, this.sx, this.sy, this.w, this.h, (this.w / -2), (this.h / -2), this.w, this.h);
+
+      //Restauration du contexte.
+      this.ctx.restore();
+
+      //On ajoute 1 à l'intervalle.
+      this.rate++;
    };
    this.Move = function() { //Fonction qui va faire bouger le vaisseau selon les touches du clavier.
-
+      //On change la position en x et en y en ajoutant la vitesse * l'angle à la position d'avant.
+      this.x += this.speed * Math.cos(this.alpha);
+      this.y += this.speed * Math.sin(this.alpha);
    };
-   this.SetAnim = function() { //Fonction qui va gérer quelle partie de l'image va appraître.
-
+   this.SetAnim = function(num) { //Fonction qui va gérer quelle partie de l'image va appraître.
+      //On prend le numéro * la hauteur et - la hauteur pour avoir par exemple la position 0 quand on donne 1 en paramètre.
+      this.sy = (num * this.h) - this.h;
+      this.sx = 0;
    };
    this.Colision = function() {
-      
+
    };
 }
