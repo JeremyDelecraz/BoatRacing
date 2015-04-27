41b645200bfa6dc99f2284679cfda1f055a65192
NicolasBoccard <nico.boccard.cfpt@gmail.com>
2015-04-27 14:03:53 +0200

    Input version final

--- BoatRacing/index.php
+++ BoatRacing/index.php
@@ -13,18 +13,23 @@
         <canvas id="canGame" width="720" height="720">
             
         </canvas>
-        <script type="text/javascript" src="script/Loader.js"></script>
-        <script type="text/javascript">
-            var listImage = {};
+        <script>
             for (i = 1; i < 31; i++)
             {
                 listImage[i-1] = "Images/Map/" + i + ".jpg";
             }
             var load = new Loader( listImage );
+            this.SetOneKey("Up", 87);
+            
+
+            input.SetOneKey("down", 83);
+            input.SetOneKey("left", 65);
+            input.SetOneKey("right", 68);   // 
+            input.SetOneKey("fire1", 16);   // shift
+            input.SetOneKey("fire2", 17);   // ctrl
         </script>
         
 <h1>Test 1</h1>
     </body>
 </html>
-<script type="text/javascript" src="script/Game.js"></script>
-<script type="text/javascript" src="script/Input.js"></script>
+<!--<script type="text/javascript" src="script/Game.js"></script>-->
--- BoatRacing/script/Input.js
+++ BoatRacing/script/Input.js
@@ -1,64 +1,72 @@
 /******************************************
-* Projet : BoatRacing
-* Description : Classe Input du projet
-* Date : 23.03.2015
-* Version : 1.0
-* Auteur : Boccard Nicolas
-* Tests : Acun test effectués
-* *****************************************/
-
+ * Projet : BoatRacing
+ * Description : Classe Input du projet
+ * Date : 23.03.2015
+ * Version : 1.0
+ * Auteur : Boccard Nicolas
+ * *****************************************/
 function Input()
 {
+    var self = this;
     this.left = false;
     this.right = false;
     this.up = false;
     this.down = false;
     this.fire1 = false;
     this.fire2 = false;
-    //this.keysCode[];
-    var keysCode = {};
-    
-    //this.onKeyUpDown = OnKeyUpDown(event);
-    
-    
-    this.ctor = function()
-    {
-        
+    this.keysCode = {};
+    /*------------------------------------------------------------------------*/
+    //Enclenche cette fonction quand on relâche une touche
+    document.onkeyup = function( event ) {
+        self.OnKeyUpDown(event);
+    };
+
+    /*------------------------------------------------------------------------*/
+    //Enclenche cette fonction quand on appuie sur une touche
+    document.onkeydown = function( event ) {
+        self.OnKeyUpDown(event);
     };
     
-    ctor();
-}
-
-function OnKeyUpDown(event)
-{
-    if (event.type === "keydown")
+    /*------------------------------------------------------------------------*/
+    //Définie le tableau associatif selon les valeurs misent en paramètres
+    this.SetOneKey = function(direction, keyCode)
     {
-        var bool = true;
-    } else
+        self.keysCode[direction] = keyCode;
+    };
+    
+    /*------------------------------------------------------------------------*/
+    //Vérifie sur quelle touche on a appuyer
+    this.OnKeyUpDown = function(event)
     {
-        var bool = false;
-    }
-    switch (event.keyCode)
-    {
-        case 87:
-            this.up = bool;
-
-            break;
-        case 83:
-            this.down = bool;
-            break;
-        case 68:
-            this.right = bool;
-            break;
-        case 65:
-            this.left = bool;
-            break;
-        default:
-    }
-}
-
-function SetOneKey(direction, keyCode)
-{
-    //keysCode[direction] = keyCode;
-    keys(direction, keyCode);
+        console.log("dans event");
+        if (event.type === "keydown")
+        {
+            var bool = true;
+        } else
+        {
+            var bool = false;
+        }
+        
+        if      ( event.keyCode == this.keysCode["up"] ) {
+                this.up = bool;
+        }
+        else if ( event.keyCode == this.keysCode["down"] ) {
+                this.down = bool;
+        }
+        else if ( event.keyCode == this.keysCode["right"] ) {
+                this.right = bool;
+        }
+        else if ( event.keyCode == this.keysCode["left"] ) {
+                this.left = bool;
+        }
+    };
+    /*
+    // Afficher l'état du clavier
+    this.getState = function() {
+        return ((this.left)  ? 'left ' : ' ') + 
+               ((this.up)    ? 'up'    : ' ') + 
+               ((this.down)  ? 'down'  : ' ') + 
+               ((this.right) ? 'right' : ' ') + 
+               '';
+    };*/
 }
