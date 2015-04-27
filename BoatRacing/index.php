fa3453e26a3d34e29d6c0fd22fb72f2503a794c3
Sebastian Lopez <sebastian.lopez.cfpt@gmail.com>
2015-04-27 13:50:23 +0200

    Chargement des images ainsi qu'ajout des images dans le dossier

--- BoatRacing/index.php
+++ BoatRacing/index.php
@@ -13,6 +13,17 @@
         <canvas id="canGame" width="720" height="720">
             
         </canvas>
+        <script type="text/javascript" src="script/Loader.js"></script>
+        <script type="text/javascript">
+            var listImage = {};
+            for (i = 1; i < 31; i++)
+            {
+                listImage[i-1] = "Images/Map/" + i + ".jpg";
+            }
+            var load = new Loader( listImage );
+        </script>
+        
+<h1>Test 1</h1>
     </body>
 </html>
 <script type="text/javascript" src="script/Game.js"></script>
--- BoatRacing/script/Loader.js
+++ BoatRacing/script/Loader.js
@@ -1,55 +1,129 @@
 /******************************************
-* Projet : Boat Racing
-* Description : Cette class va se charger de les images et de les mettres à disposition.
-* Date : 23.03.2015
-* Version : 1.0
-* Auteur : Sebastian Lopez
-* Tests : 
-* *****************************************/
-var self = this;
-function ObjLoader()
+ * Projet : Boat Racing
+ * Description : Cette class va se charger de les images et de les mettres à disposition.
+ * Date : 23.03.2015
+ * Version : 1.0
+ * Auteur : Sebastian Lopez
+ * Tests : 
+ * *****************************************/
+
+
+function Loader(imgList)
 {
-    this.images = array(id,Url),
-    this.total,
-    this.error,
-    this.count,
-    this.message;
+   var self = this;
+
+   this.images = {};
+   this.total = 0;
+   this.error = 0;
+   this.count = 0;
+   this.message = "";
+
+
+   // Constructeur ------------------------------------------------------------
+
+   for (var item in imgList)
+   {
+      var img = new Image();
+
+      img.src = imgList[item];
+      self.images[item] = img;
+      self.count++;
+
+      img.onload = function()
+      {
+         self.total += 1;
+         if (self.total == self.count)
+         {
+            alert("C'est bon");
+         }
+         //alert("Ca charge");
+      }
+
+      img.onerror = function()
+      {
+         self.error += 1;
+         self.message = "Erreur lors du chargement de l'image ";
+         //alert(imgList);
+         //alert("C'est nul");
+
+         if (self.total != self.count)
+         {
+            alert("Toutes les images n'ont pas chargé");
+         }
+      }
+      //console.log( "id = " + listOne + "url = " + imgList[listOne] );
+   }
+
+   function Loaded()
+   {
+      if (load.count === load.Total)
+      {
+         return true;
+      }
+      else
+      {
+         return false;
+      }
+   }
+
+   function GetImage(id)
+   {
+      if (image[id] == null)
+      {
+         return null;
+      }
+      else
+      {
+         return image[id];
+      }
+   }
+
 }
 
-function Loader(list)
-{
-    load = new ObjLoader();
-    var listOne = list;
-    
-    var img = new Image();
-    
-    
-    img.onload = function ()
-    {
-        load.count += 1;
-    };
-    
-    img.onerror = function ()
-    {
-        load.message = "Erreur de chargement d'image";
-    };
-    
-    img.src = listOne.url;
-    
-    this.image[listOne.id] = img;
-}
 
-function Loaded()
-{
-    
-}
-
-function GetImage(id)
-{
-    for(i = 0; i<load.total; i++)
-    {
-        return load.image[id];
-    }
-}
-
-
+/*
+ function Loader(list)
+ {
+ 
+ return 0;
+ 
+ 
+ load = new ObjLoader();
+ var listOne = list;
+ 
+ var img = new Image();
+ 
+ 
+ img.onload = function ()
+ {
+ load.count += 1;
+ };
+ 
+ img.onerror = function ()
+ {
+ load.message = "Erreur de chargement d'image";
+ };
+ 
+ img.src = listOne.url;
+ 
+ this.image[listOne.id] = img;
+ }
+ 
+ function Loaded()
+ {
+ if(load.count === load.Total)
+ {
+ return true;
+ }
+ else
+ {
+ return false;
+ }
+ }
+ 
+ function GetImage(id)
+ {
+ return load.image[id];
+ }
+ 
+ */
