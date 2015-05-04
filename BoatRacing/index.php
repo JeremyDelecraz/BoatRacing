09c7841e65438bd023fe827d58097bb34b2ed407
NicolasBoccard <nico.boccard.cfpt@gmail.com>
2015-05-04 14:45:31 +0200

    Ajout de la sélection du pseudo

--- BoatRacing/index.php
+++ BoatRacing/index.php
@@ -1,4 +1,27 @@
-<?PHP ?>
+<?PHP
+
+
+
+if (isset($_POST['submit_pseudo']) && $_POST['input_pseudo'])
+{
+    if(strlen($_POST['input_pseudo']) > 3)
+    {
+        if(strlen($_POST['input_pseudo']) < 10)
+        {
+            $_POST['pseudoOk'] = TRUE;
+        }
+        else
+    {
+        echo "Le pseudo est trop grand (maximum 10 caractères)";
+    }
+    }
+    else
+    {
+        echo "Le pseudo est trop petit (minimum 4 caractères)";
+    }
+}
+
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -9,16 +32,28 @@
             border: 2px black solid;
         }
     </style>
-    <body onload="vgGame = new Game(document.getElementById('canGame'));">
-        <input type="hidden" id="localid"  value="">
-        <canvas id="canGame" width="640" height="384">
+    <?php 
+    
+    if(isset($_POST['pseudoOk']) && $_POST['pseudoOk'] == TRUE)
+    {
+        echo "<body onload=\"vgGame = new Game(document.getElementById('canGame'));\">
+        <canvas id=\"canGame\" width=\"640\" height=\"384\">
 
         </canvas>
     </body>
-    <script type="text/javascript" src="script/Game.js"></script>
-    <script type="text/javascript" src="script/Input.js"></script>
-    <script type="text/javascript" src="script/Loader.js"></script>
-    <script type="text/javascript" src="script/Items.js"></script>
-    <script type="text/javascript" src="script/Sprite.js"></script>
-    <script type="text/javascript" src="script/Map.js"></script>
+    <script type=\"text/javascript\" src=\"script/Game.js\"></script>
+    <script type=\"text/javascript\" src=\"script/Input.js\"></script>
+    <script type=\"text/javascript\" src=\"script/Loader.js\"></script>
+    <script type=\"text/javascript\" src=\"script/Items.js\"></script>
+    <script type=\"text/javascript\" src=\"script/Sprite.js\"></script>
+    <script type=\"text/javascript\" src=\"script/Map.js\"></script>";
+    }
+ else {
+        echo "<form action=\"index.php\" method=\"post\">
+        <input type=\"text\" name=\"input_pseudo\"/>
+        <input type=\"submit\" name=\"submit_pseudo\" value=\"Entrer\"/>
+        </form>";
+    }
+    
+    ?>
 </html>
