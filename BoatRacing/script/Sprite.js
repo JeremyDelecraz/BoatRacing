afde40bb84057f210867a1f4719c0c6ff16ec7fc
Franfrandudu <francois.duvernaycfpt@gmail.com>
2015-05-11 13:47:56 +0200

    Correction / ajout commentaire classe Sprite.js

--- BoatRacing/script/Sprite.js
+++ BoatRacing/script/Sprite.js
@@ -18,10 +18,10 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
    this.sy = anim; //La position en y dans l'image.
    this.rate = rate; //La vitesse de changement de sprite (paramètre).
    this.radius = this.w / 2; //Le rayon du sprite pour calculer une hitbox.
-   this.energy = 100; //Vie du sprite
+   this.energy = 100; //Vie du sprite.
    this.update; //Pour le réseau.
-   this.hurt = false; //Définni si le bateau c'est fait touché ou non.
-   this.shield = 0; //Bouclier pour quand deux bateau se rentre dedans.
+   this.hurt = false; //Définni si le bateau s'est fait touché ou non.
+   this.shield = 0; //Bouclier pour quand deux bateaux se rentrent dedans.
    this.ctx = ctx; //Contexte dans lequel va se trouver l'image (paramètre).
    this.Show = function() { //Fonction pour afficher le sprite.
       //Si l'intervalle est égale à 100, on change la position dans le sprite.
@@ -38,9 +38,9 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
          //On remet l'intervalle à 0.
          this.rate = 0;
       }
-      //On baise le numéro du bouclier.
+      //On baisse le numéro du bouclier.
       this.shield--;
-      //Sauvegarde du contex te.
+      //Sauvegarde du contexte.
       this.ctx.save();
       this.ctx.translate(this.x, this.y);
 
@@ -55,17 +55,17 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
       this.rate++;
    };
    this.Move = function() { //Fonction qui va faire bouger le vaisseau selon les touches du clavier.
-      //On change la position en x et en y en ajoutant la vitesse * l'angle à la position d'avant.
+      //On change la position en x et en y en ajoutant la vitesse multipliée par l'angle à la position d'avant.
       this.x += this.speed * Math.cos(this.alpha);
       this.y += this.speed * Math.sin(this.alpha);
    };
    this.SetAnim = function(num) { //Fonction qui va gérer quelle partie de l'image va appraître.
-      //On prend le numéro * la hauteur et - la hauteur pour avoir par exemple la position 0 quand on donne 1 en paramètre.
+      //On prend le numéro muliplié par la hauteur et moins la hauteur pour avoir par exemple la position 0 quand on donne 1 en paramètre.
       this.sy = (num * this.h) - this.h;
       this.sx = 0;
    };
    this.Colision = function(opponentBoat) { //Fonction qui va gérer la collision, prend en paramètre un bateau adversaire.
-      //On fait pythagore pour savoir si le rayon de l'un va dans l'autre.
+      //On fait pythagore pour savoir si le rayon de l'un va dans le rayon de l'autre.
       var pythagore = Math.sqrt(Math.pow((this.x - opponentBoat.x), 2) + Math.pow((this.y - opponentBoat.y), 2));
 
       if ((pythagore <= (this.radius + opponentBoat.radius)) && (this.shield <= 0) && (opponentBoat.shield <= 0))
@@ -73,7 +73,7 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
          return true;
       }
    };
-   this.IsDead = function() //Cette fonction va permettre de vérifier si le bateau est mort ou non selon l'énergie
+   this.IsDead = function() //Cette fonction va permettre de vérifier si le bateau est mort ou non selon l'énergie.
    {
       if (this.energy <= 0)
       {
@@ -86,6 +86,7 @@ function Sprite(ctx, img, x, y, frame, anim, rate) {
    };
    this.EndAnim = function() //Cette fonction va vérifier si on arrive à la fin de l'animation ou non.
    {
+      //Note : 96 est la taille de l'image en largeur.
       if (this.sx === 96)
       {
          return true;
